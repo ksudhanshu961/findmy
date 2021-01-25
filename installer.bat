329fe063a117bde3d35a708bac88341ba8e1fb4d
ksudhanshu961 <66401925+ksudhanshu961@users.noreply.github.com>
2021-01-25 21:09:08 +0530

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,3 +1,3 @@
 @echo off
 
-mkdir C:\Program Files (x86)\MyFindMy & copy findmy.exe "C:\Program Files (x86)\MyFindMy\"
+mkdir C:\Program Files (x86)\MyFindMy & move findmy.exe "C:\Program Files (x86)\MyFindMy\"
